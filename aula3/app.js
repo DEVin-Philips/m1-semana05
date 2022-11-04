d2fa64b3388b9a8b6f26d44e4513b5321561e985
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-11-03 21:53:33 -0300

    atualiza exemplo m1-s05-a3

--- aula3/app.js
+++ aula3/app.js
@@ -1,30 +1,9 @@
-import Pessoa from "./Pessoa.js";
+import Animal from "./Animal.js";
+import Cachorro from "./Cachorro.js";
 
-class Animal {
-  #nome
-  constructor(nome) {
-    this.#nome = nome;
-  }
-  getMetodoComum() {
-    return `Id: ${this.#nome}`;
-  }
-  setMetodoComum(valor) {
-    console.log({ valor });
-    this.#nome = valor;
-  }
-  get nome() {
-    console.log("GETTER");
-    // adicione aqui sua lógica...
-    return `Id: ${this.#nome}`;
-  }
-  set nome(valor) {
-    console.log("SETTER");
-    // adicione aqui sua lógica...
-    this.#nome = valor;
-  }
-}
-const instancia = new Animal("Preguiça");
+const instancia = new Animal("Preguiça", "ZzzZZ");
 
+/*
 //console.log(instancia.#nome);
 console.log(instancia.getMetodoComum());
 console.log(instancia.nome);
@@ -34,3 +13,11 @@ console.log(instancia.nome);
 
 instancia.nome = "Jaguatirica";
 console.log(instancia.nome);
+*/
+
+const dogginho = new Cachorro("Viralata");
+
+//console.log(dogginho.nome)
+
+dogginho.teste()
+
