ef6bee815c3f640d06e01c20e902c96a1acaca90
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-11-04 20:27:33 -0300

    adiciona exercicio 3 m1-s05

--- exercicios/e3/app.js
+++ exercicios/e3/app.js
@@ -1,1 +1,15 @@
-console.log('m1-s05-e1')
+import Personagem from './Personagem.js'
+
+console.log('m1-s05-e3')
+
+const alyx = new Personagem("Alyx");
+
+console.log(alyx.percentualVida); // 100
+
+alyx.sofreuDano(50);
+
+console.log(alyx.percentualVida); // 50
+
+alyx.usouKitMedico();
+
+console.log(alyx.percentualVida); // 60
