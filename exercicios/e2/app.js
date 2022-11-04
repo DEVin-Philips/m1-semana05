9814387493a2fe4aadf5e0a3f3d0d0141f3181fb
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-11-04 20:12:22 -0300

    adiciona exercicio 2 m1-s05

--- exercicios/e2/app.js
+++ exercicios/e2/app.js
@@ -1,1 +1,12 @@
-console.log('m1-s05-e1')
+import Fatura from './Fatura.js'
+
+console.log('m1-s05-e2')
+
+
+const melao = new Fatura(123, "Melão", 2, 3);
+
+const valor = melao.obterValorTotal();
+
+console.log(valor); // 6
+
+console.log(melao.valorTotal); // 6
