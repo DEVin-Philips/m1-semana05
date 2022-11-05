ebac31d61cc4d8568775c9ed2e1b90153e0b08fd
romeu-senai <romeu.casarotto@edu.sc.senai.br>
2022-11-04 21:53:14 -0300

    adiciona exercicio 4 m1-s05

--- exercicios/e4/app.js
+++ exercicios/e4/app.js
@@ -1,2 +1,10 @@
+import Cachorro from './Cachorro.js'
+import Gato from './Gato.js'
 
 console.log('m1-s05-e4')
+
+const snoopy = new Cachorro('snoopy', 10);
+console.log( snoopy.som );   // "latido"
+
+const frajola = new Gato('frajola', 8);
+console.log( frajola.som );   // "miado"
